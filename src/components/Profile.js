7f72e1f1d0400bcf194776d98576bc3996aba975
TakashimaY <yousukes53@gmail.com>
2024-09-19 12:40:35 +0900

    bug fixed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Heading, Text } from '@chakra-ui/react';
-const profile = () => {
+const Profile = () => {
     return (
         <Box p={5} shadow="md" borderWidth="1px">
             <Heading fontSize="xl">Your Name</Heading>
