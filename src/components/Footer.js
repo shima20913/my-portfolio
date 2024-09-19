a9e91d6528897c3962b2475339eb5e1d230f7af6
TakashimaY <yousukes53@gmail.com>
2024-09-19 13:39:55 +0900

    removed Text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box,} from '@chakra-ui/react';
 
 const Footer = () => {
   return (
     <Box bg="teal.500" color="white" py={4} textAlign="center">
-      <Text>© 2024 . All rights reserved.</Text>
     </Box>
   );
 };
