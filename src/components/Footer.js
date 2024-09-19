28c792a146a921f6469f9a6a0dbb87de61927d9e
TakashimaY <yousukes53@gmail.com>
2024-09-19 12:41:57 +0900

    implement footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+const Footer = () => {
+  return (
+    <Box bg="teal.500" color="white" py={4} textAlign="center">
+      <Text>© 2024 Your Name. All rights reserved.</Text>
+    </Box>
+  );
+};
+
+export default Footer;
