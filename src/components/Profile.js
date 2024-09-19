85855a134ceeace89331f1e73e372d07b7add3d9
TakashimaY <yousukes53@gmail.com>
2024-09-19 12:37:39 +0900

    implement profile

---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Heading, Text } from '@chakra-ui/react';
+const profile = () => {
+    return (
+        <Box p={5} shadow="md" borderWidth="1px">
+            <Heading fontSize="xl">Your Name</Heading>
+            <Text mt={4}>プロフィール文を挿入。</Text>
+        </Box>
+    );
+};
+
+    export default Profile;
