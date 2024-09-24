b992206cc647aa458a52c9e122e2cbe3e361668f
TakashimaY <yousukes53@gmail.com>
2024-09-24 15:14:17 +0900

    fixed UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import { auth } from '../firebase';
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { 
+  Container, 
+  Box, 
+  VStack, 
+  Heading, 
+  FormControl, 
+  FormLabel, 
+  Text,
+  Button,
+} from '@chakra-ui/react';
 
 
 const Login = ({ setUser }) => {
@@ -20,21 +30,39 @@ const Login = ({ setUser }) => {
     };
 
     return (
-        <div>
-          <h2>ログイン</h2>
+      <Container maxW="sm" centerContent>
+        <Box
+        p={6}
+        mt={12}
+        w="100%"
+        maxW="md"
+        borderWidth={1}
+        borderRadius="lg"
+        boxShadow="lg"
+        bg="white"
+      >
+        
+         <VStack spacing={5} align="stretch">
+         <Heading as="h2" size="lg" textAlign="center" color="teal.500">
+          ログイン
+          </Heading>
           <form onSubmit={handleSubmit}>
-            <div>
-              <label>Email</label>
-              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            </div>
-            <div>
-              <label>Password</label>
-              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <button type="submit">ログイン</button>
-          </form>
-          {error && <p>{error}</p>}
-        </div>
+          <FormControl id="email" isRequired>
+              <FormLabel>Email</FormLabel>
+              <input type="email" placeholder="メールアドレスを入力" value={email} onChange={(e) => setEmail(e.target.value)} />
+              </FormControl>
+
+              <FormControl id="password" isRequired mt={4}>
+              <FormLabel>Password</FormLabel>
+              <input type="password" placeholder="パスワードを入力" value={password} onChange={(e) => setPassword(e.target.value)} focusBorderColor="teal.400"/>
+              </FormControl>
+            <Button  variant="solid" type="submit" colorScheme="blue" size="md"  mt={6} w="full" _hover={{ bg: "teal.600" } }>ログイン</Button>
+            </form>
+
+            {error && <Text color="red.500" textAlign="center">{error}</Text>}
+            </VStack>
+            </Box>
+            </Container>
       );
     };
     
