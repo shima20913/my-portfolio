b38621e60de0a3376b98de0310f54a255dc33b23
TakashimaY <yousukes53@gmail.com>
2024-09-24 18:04:15 +0900

    Changed so that the blog can be edited if the administrator logs in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
+import { auth, firestore } from './firebase';
+import { doc, getDoc } from 'firebase/firestore';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/react';
@@ -9,11 +12,27 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
 import Github from './components/Github';
-import BlogPage from './components/BlogPage';  
+import BlogList from './components/BlogList';  
 import Login from './pages/Login'; 
+import ProtectRoute from './components/ProtectRoute';
+import BlogEdit from './components/BlogEdit';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      const fetchUserRole = async () => {
+        const userDocRef = doc(firestore, "users", user.uid);
+        const userDoc = await getDoc(userDocRef);
+        if (userDoc.exists() && userDoc.data().isAdmin) {
+          setIsAdmin(true); 
+        }
+      };
+      fetchUserRole();
+    }
+  }, [user]);
 
   return (
     <ChakraProvider>
@@ -26,7 +45,10 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/github" element={<Github />} />
-            <Route path="/myblog" element={<BlogPage />} /> 
+            <Route path="/blog" element={<BlogList />} />
+            <Route path="/admin/blog-editor" element={<ProtectRoute user={user} isAdmin={isAdmin}><BlogEdit /></ProtectRoute>
+                }
+              />
             <Route path="/login" element={<Login setUser={setUser} />} />
           </Routes>
         </Box>
