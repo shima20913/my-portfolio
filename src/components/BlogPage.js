33f22a539e9c36e729c1821d64a61e07263513bb
TakashimaY <yousukes53@gmail.com>
2024-09-24 13:00:03 +0900

    add blog page

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -6,4 +6,21 @@ const BlogPage = () => {
    const addBlogEntry = (entry) => {
     setEntries([entry, ...entries]);
    };
-}
+
+   return (
+    <Box p={5}>
+      <Heading as="h1" mb={5}>
+        投稿する
+      </Heading>
+      <DiaryForm addDiaryEntry={addBlogEntry} />
+      <Heading as="h2" mt={10} mb={5}>
+        投稿されたブログ
+      </Heading>
+      <DiaryList entries={entries} />
+    </Box>
+  );
+};
+
+export default BlogPage;
+
+
