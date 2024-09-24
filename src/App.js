0762cfe5441bba69e2d03de8d70b2e87c4d7cf99
TakashimaY <yousukes53@gmail.com>
2024-09-24 13:21:23 +0900

    add route to blog page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
 import Github from './components/Github';
+import BlogPage from './components/BlogPage';  
 
 function App() {
   return (
+    <ChakraProvider>
     <Router>
       <Box>
         <Header />
@@ -19,11 +22,13 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/github" element={<Github />} />
+            <Route path="/my blog/:id" element={<BlogPage />} /> 
           </Routes>
         </Box>
         <Footer />
       </Box>
     </Router>
+    </ChakraProvider>
   );
 }
 
